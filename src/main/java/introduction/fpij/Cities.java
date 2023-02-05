bedc99a556693b289e698252784b68dedf080fe7
冨澤宝斗 <takatty@takattyM2MBP.local>
2023-02-05 23:01:01 +0900

    before: 命令型コード

---
+++ src/main/java/introduction/fpij/Cities.java
@@ -0,0 +1,18 @@
+package introduction.fpij;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Cities {
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("Albany", "Boulder", "Chicago", "Denver", "Eugene");
+        boolean flag = false;
+        for (String city : cities) {
+            if (city.equals("Chicago")) {
+                flag = true;
+                break;
+            }
+        }
+        System.out.println("contain Chicago? : " + flag);
+    }
+}
